b7ead9a4f3a269a02cbe05a2632cb541ac5bdbe1
ashwinraj8090 <ashwinraj8090@gmail.com>
2026-05-10 22:24:00 +0530

    Fix CSRF for admin login

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-$^3-qmxil8etmshqz2$ocri^3u%ww2%*-dm@*@9z$nmu=+bur@')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host != '*']
+if '*' in ALLOWED_HOSTS:
+    CSRF_TRUSTED_ORIGINS.append("https://*.railway.app")
+
 
 
 
